ekf added. Notes: -- might be under estimating variance -- but formulas seem correct

slam/fastslam.py
@@ -8,7 +8,7 @@ from matplotlib.collections import PathCollection
 
 from slam.map import Map
 from slam.action_model import ActionModelSettings, action_model
-from slam.ekf import EKFSettings
+from ekf.ekf import EKFSettings
 from slam.resampling import ResampleType
 from slam.particle import Particle
 
@@ -105,7 +105,7 @@ class FastSLAM:
     def _draw_map(self, actual_map = None) -> None:
         particle_idx_for_map = np.argmax(np.array([particle.weight for particle in self.particles]), axis=0)
         map_estimate = self.particles[particle_idx_for_map].map.landmarks
-        landmark_positions = [map_estimate[landmark].get_position() for landmark in map_estimate]
+        landmark_positions = [map_estimate[landmark].get_mu() for landmark in map_estimate]
         print(landmark_positions)
         self.landmark_dots.set(offsets = landmark_positions)
         self._draw()

slam/map.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from slam.ekf import EKF
+from ekf.ekf import EKF
 
 print(f"[WARNING] Map code does not behave as Map.")
 class Map:
@@ -12,7 +12,7 @@ class Map:
             self.landmarks[landmark_id] = EKF(landmark_position)
             return 1.0
         else:
-            prev_loc = self.landmarks[landmark_id].get_position()
+            prev_loc = self.landmarks[landmark_id].get_mu()
             self.landmarks[landmark_id].update(landmark_position)
             return 1.0 / (np.linalg.norm(prev_loc - landmark_position) + 1)
 