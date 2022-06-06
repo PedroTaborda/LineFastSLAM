added images, fixed sensor data odom angle units

sensor_data/sensor_data.py
@@ -31,9 +31,13 @@ class SimulationData:
 
 @dataclass
 class SensorData:
-    odometry: list[tuple[int, np.ndarray]]        # (timestamp, [theta, x, y])
-    lidar: list[tuple[int, np.ndarray]]           # (timestamp, [phi, r])
-    camera: list[tuple[int, list[tuple[int, np.ndarray]]]]           # (timestamp, list[id, [phi, r]])
+    odometry: list[tuple[int, np.ndarray]]                  # (timestamp, [theta, x, y])
+    lidar: list[tuple[int, np.ndarray]]                     # (timestamp, [phi, r])
+    # camera is: (timestamp, list[id, [phi, r]], CompressedImg)
+    # decompress example: Img = cv2.imdecode(CompressedImg, cv2.IMREAD_COLOR)
+    camera: list[tuple[int, list[tuple[int, np.ndarray]], np.ndarray]]
+
+
     comment: str = ''
     from_rosbag: bool = False
     sim_data: SimulationData = None
@@ -81,7 +85,7 @@ def detect_landmarks(image: np.ndarray, camera_matrix: np.ndarray, distortion_co
 
         # Use the position of the markers to get the distance and difference in heading to the robot
         distance = np.linalg.norm(translation)
-        angle = np.rad2deg(np.arctan(-translation[0]/translation[2]))
+        angle = np.arctan(-translation[0]/translation[2])
         angles.append(angle)
         distances.append(distance)
 
@@ -101,13 +105,15 @@ def detect_landmarks(image: np.ndarray, camera_matrix: np.ndarray, distortion_co
 
         cv2.putText(image, f'd: {distance:.3f}', (topLeft[0] - 50, topLeft[1] - 30), cv2.FONT_HERSHEY_SIMPLEX,
                     0.5, (0, 255, 0), 2)
-        cv2.putText(image, f'angle: {angle:.1f}', (topLeft[0] - 50, topLeft[1] - 15), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5, (0, 255, 0), 2)
+        cv2.putText(
+            image, f'angle: {np.rad2deg(angle):.1f}', (topLeft[0] - 50, topLeft[1] - 15),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0),
+            2)
 
-    return (image, list(zip([id[0] for id in ids], [(distance, np.deg2rad(angle)) for angle, distance in zip(angles, distances)])))
+    return (image, list(zip([id[0] for id in ids], [(distance, angle) for angle, distance in zip(angles, distances)])))
 
 
-def rosbag_to_data(rosbag_path: os.PathLike) -> SensorData:
+def rosbag_to_data(rosbag_path: os.PathLike, save_imgs=False) -> SensorData:
     laser_ros = []
     odom_ros = []
     cam_ros_sim = []
@@ -138,7 +144,7 @@ def rosbag_to_data(rosbag_path: os.PathLike) -> SensorData:
         if len(connections_odom) != 0:
             for connection, timestamp, rawdata in reader.messages(connections=connections_odom):
                 msg = rosbags.serde.deserialize_cdr(rosbags.serde.ros1_to_cdr(rawdata, connection.msgtype), connection.msgtype)
-                theta, x, y = msg.pose.pose.orientation.z*180.0/np.pi, msg.pose.pose.position.x, msg.pose.pose.position.y
+                theta, x, y = msg.pose.pose.orientation.z, msg.pose.pose.position.x, msg.pose.pose.position.y
                 odom_ros.append((timestamp, np.array([theta, x, y])))
         if len(connections_cam_sim) != 0:
             raise NotImplementedError('Camera on simulation not implemented')
@@ -154,8 +160,12 @@ def rosbag_to_data(rosbag_path: os.PathLike) -> SensorData:
             for connection, timestamp, rawdata in reader.messages(connections=connections_cam):
                 msg = rosbags.serde.deserialize_cdr(rosbags.serde.ros1_to_cdr(rawdata, connection.msgtype), connection.msgtype)
                 img = cv2.imdecode(msg.data, cv2.IMREAD_COLOR)
-                _, landmarks = detect_landmarks(img, camera_matrix, distortion_coefficients)
-                cam_ros_real.append((timestamp, landmarks))
+                annotated_img, landmarks = detect_landmarks(img, camera_matrix, distortion_coefficients)
+                Compressed_Annotated = None
+                if save_imgs:
+                    Compressed_Annotated = cv2.imencode('.jpeg', annotated_img)[1]
+                cam_ros_real.append((timestamp, landmarks, Compressed_Annotated))
+
 
     return SensorData(odometry=odom_ros, lidar=laser_ros, camera=cam_ros_real, comment='From rosbag', from_rosbag=True)
 
@@ -215,11 +225,12 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Convert sensor data from rosbag to sensor data')
     parser.add_argument('--rosbag', type=str, help='Path to rosbag file', required=True)
+    parser.add_argument('--save_images', action="store_true")
 
     args = parser.parse_args()
 
     if args.rosbag:
-        sensor_data = rosbag_to_data(args.rosbag)
+        sensor_data = rosbag_to_data(args.rosbag, args.save_images)
         lst = os.path.basename(args.rosbag).split('.')
         lst[-1] = lst[-1].replace('bag', 'xz', 1)
         sensor_data.save('.'.join(lst))

slam/action_model.py
@@ -13,7 +13,7 @@ class UncertaintyType(Enum):
 
 @dataclass
 class ActionModelSettings:
-    uncertainty_type: UncertaintyType = UncertaintyType.POSE_ADD
+    uncertainty_type: UncertaintyType = UncertaintyType.ODOM_MULT
 
     POSE_ADD_MU: np.ndarray = np.zeros((3,))
     POSE_ADD_COV: np.ndarray = np.square(np.diag([0.01, 0.01, 0.5*np.pi/180]))

slam/fastslam.py
@@ -93,7 +93,8 @@ class FastSLAM:
         """Resamples the particles based on their weights.
         """
         weights = np.array([particle.weight for particle in self.particles])
-        self.particles = self.settings.resampling_type(self.particles, weights, self.settings.num_particles)
+        if any(weights - weights[0] != 0):
+            self.particles = self.settings.resampling_type(self.particles, weights, self.settings.num_particles)
 
     def pose_estimate(self) -> np.ndarray:
         """Returns the estimated location of the robot.
@@ -122,6 +123,7 @@ class FastSLAM:
     # Visualization methods (if settings.visualize is True)
     def _init_visualizer(self, ylim: tuple=(-3, 3), xlim: tuple=(-3, 3)) -> None:
         self.actual_location_dot: PathCollection = self.ax.scatter(0, 0, marker='x', c='k', alpha=0.0)
+        self.drawn_map_estimate = self.particles[0].map  # guess map just to avoid initializing to None
         self.ax.set_xlim(*xlim)
         self.ax.set_ylim(*ylim)
 
@@ -156,8 +158,12 @@ class FastSLAM:
 
     def _draw_map(self) -> None:
         particle_idx_for_map = np.argmax(np.array([particle.weight for particle in self.particles]), axis=0)
-        map_estimate: Map = self.particles[particle_idx_for_map].map
-        map_estimate._draw(self.ax, color_ellipse='C01', color_p='C01', color_z='C01')
+        new = self.particles[particle_idx_for_map].map
+        if self.drawn_map_estimate is not new:
+            self.drawn_map_estimate._undraw()
+            self.drawn_map_estimate = new
+
+        self.drawn_map_estimate._draw(self.ax, color_ellipse='C01', color_p='C01', color_z='C01')
         self._draw()
 
     def _draw(self) -> None:

slam/offline.py
@@ -4,23 +4,32 @@ from matplotlib import pyplot as plt
 
 import numpy as np
 from visualization_utils.mpl_video import to_video
+import cv2
 
 import slam.fastslam as fs
 import sensor_data.sensor_data as sd
 import usim.umap
 
+
 def there_is_data(data: sd.SensorData, idx_lidar, idx_camera, idx_odometry):
     return idx_lidar < len(data.lidar) or idx_camera < len(data.camera) or idx_odometry < len(data.odometry)
 
-def slam_sensor_data(data: sd.SensorData, slam_settings: fs.FastSLAMSettings = fs.FastSLAMSettings(), images_dir = None, realtime: bool = False):
+
+def slam_sensor_data(data: sd.SensorData, slam_settings: fs.FastSLAMSettings = fs.FastSLAMSettings(),
+                     images_dir=None, realtime: bool = False, show_images: bool = False):
     if slam_settings.visualize is False:
         raise ValueError('Visualization must be enabled to use slam_sensor_data.')
-    fig, axes = plt.subplots(1, 1, figsize=(10, 5), sharex=True, sharey=True)
+
+    if show_images:
+        fig, (axes, cam_ax) = plt.subplots(2, 1, figsize=(10, 5))
+    else:
+        fig, axes = plt.subplots(1, 1, figsize=(10, 5), sharex=True, sharey=True)
     axes: plt.Axes
     slammer = fs.FastSLAM(slam_settings, axes)
     i = j = -1
     k = 0
-    t0_ros = [data.lidar[i+1][0], data.camera[j+1][0], data.odometry[k+1][0]][np.argmin([data.lidar[i+1][0], data.camera[j+1][0], data.odometry[k+1][0]])]
+    t0_ros = [data.lidar[i+1][0], data.camera[j+1][0], data.odometry[k+1][0]
+              ][np.argmin([data.lidar[i+1][0], data.camera[j+1][0], data.odometry[k+1][0]])]
 
     def next_times():
         tlidar = data.lidar[i+1][0] if i+1 < len(data.lidar) else np.inf
@@ -49,9 +58,8 @@ def slam_sensor_data(data: sd.SensorData, slam_settings: fs.FastSLAMSettings = f
             else:
                 it = np.argmin(next_times())
                 t = next_times()[it]/1e9
-                
 
-            if data.sim_data is not None:
+            if data.sim_data is not None and show_images:
                 sim_i = round(t/ts)
                 if sim_i >= sim_N:
                     break
@@ -59,24 +67,28 @@ def slam_sensor_data(data: sd.SensorData, slam_settings: fs.FastSLAMSettings = f
                 estimated_pose = slammer.pose_estimate()
 
             t0 = time.time()
-            if it == 0: # Lidar data incoming
+            if it == 0:  # Lidar data incoming
                 i += 1
-            elif it == 1: # Camera data incoming
+            elif it == 1:  # Camera data incoming
                 j += 1
-                for obs in data.camera[j][1]:
-                    id, landmark = obs
-                    r, theta = landmark
+                _, landmarks, CmpImg = data.camera[j]
+                if CmpImg is not None:
+                    cam_ax.clear()
+                    Img = cv2.imdecode(CmpImg, cv2.IMREAD_COLOR)
+                    cam_ax.imshow(cv2.cvtColor(Img, cv2.COLOR_BGR2RGB))
+                for id, z in landmarks:
+                    r, theta = z
                     slammer.make_observation(t, (id, np.array([r, theta])))
-            elif it == 2: # Odometry data incoming
+            elif it == 2:  # Odometry data incoming
                 k += 1
                 theta0, x0, y0 = data.odometry[k-1][1]
                 theta1, x1, y1 = data.odometry[k][1]
                 odom = np.array([np.sqrt((x1-x0)**2 + (y1-y0)**2), (theta1-theta0)]).squeeze()
+                slammer.resample()
                 if data.sim_data is not None:
                     slammer.perform_action(t, odom, actual_pose)
                 else:
                     slammer.perform_action(t, odom)
-                slammer.resample()
 
                 if images_dir is not None:
                     plt.savefig(os.path.join(images_dir, f"{k:06d}.png"))
@@ -91,17 +103,18 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--not-realtime", action="store_true")
+    parser.add_argument('--show_images', action="store_true")
     parser.add_argument("--no-visualize", action="store_true")
     parser.add_argument("--file", type=str, default='sim0.xz')
     parser.add_argument("-N", type=int, default=50)
     parser.add_argument("--images-dir", type=str, default='images_slam')
-    
+
     args = parser.parse_args()
 
     images_dir = os.path.join('data', args.images_dir)
     if not os.path.isdir(images_dir):
         os.mkdir(images_dir)
-    
+
     slam_sensor_data(
         sd.load_sensor_data(args.file),
         slam_settings=fs.FastSLAMSettings(
@@ -110,5 +123,6 @@ if __name__ == "__main__":
             trajectory_trail=True,
         ),
         realtime=not args.not_realtime,
-        images_dir=images_dir
+        images_dir=images_dir,
+        show_images=args.show_images
     )