1ce4fe73cfdbedd448691637982d20da18060986
Pedro Taborda <33559524+PedroTaborda@users.noreply.github.com>
2022-06-21 15:26:06 +0100

    Fixed some bugs and improved return type

--- slam/mass.py
+++ slam/mass.py
@@ -7,6 +7,9 @@ import slam.fastslam as fs
 import slam.offline as offline
 import sensor_data.sensor_data as sd
 
+if not os.path.isdir('data'):
+    os.mkdir('data')
+
 def file_name(settings: fs.FastSLAMSettings) -> str:
     """
     Generate a file name for the given settings.
@@ -29,20 +32,22 @@ def perform_slam(sensor_and_settings_obj: tuple[sd.SensorData, fs.FastSLAMSettin
     return res
 
 def slam_batch(settings: list[fs.FastSLAMSettings], sensor_data: sd.SensorData, 
-                repeats: int = 5, pool_processes: int = 4, results_dir = 'slammed') -> list[fs.FastSLAM]:
+                repeats: int = 2, pool_processes: int = 4, results_dir = 'slammed') -> list[fs.FastSLAM]:
     """
     Run FastSLAM on a batch of settings, 'repeats' times per settings object.
-    """
-    if not repeats:
-        expanded_settings = settings
-    else:
-        expanded_settings = []
-        for s in settings:
-            for i in range(repeats):
-                new_settings = copy.copy(s)
-                new_settings.rng_seed = i
-                expanded_settings.append(s)
+    Returns a list of lists of fs.SLAMResult objects.
+    Each list contains the results for one settings object for several seeds.
 
+    These results are stored in files, indexed by the settings object hash. # TODO: add hashing of sensor data object to make them unique.
+    """
+    expanded_settings = []
+    for s in settings:
+        for i in range(repeats):
+            new_settings = copy.copy(s)
+            new_settings.rng_seed = i
+            expanded_settings.append(new_settings)
+    
+    results_dir = os.path.join('data', results_dir)
     if not os.path.isdir(results_dir):
         os.mkdir(results_dir)
     n_processed = 0
@@ -60,15 +65,22 @@ def slam_batch(settings: list[fs.FastSLAMSettings], sensor_data: sd.SensorData,
     results = pool.map(perform_slam, to_process)
 
     for s, res in zip(to_process, results):
+        print(f"Saving results for {file_name(s[1])}")
         rel_path = os.path.join(results_dir, file_name(s[1]))
         with open(rel_path, 'wb') as f:
             pickle.dump(res, f)
 
     res_ret = []
-    for settings in expanded_settings:
-        rel_path = os.path.join(results_dir, file_name(settings))
-        with open(rel_path, 'rb') as f:
-            res_ret.append(pickle.load(f))
+    for s in settings:
+        res_settings_lst = []
+        for i in range(repeats):
+            print(f"Loading results for {file_name(s)}")
+            new_settings = copy.copy(s)
+            new_settings.rng_seed = i
+            rel_path = os.path.join(results_dir, file_name(new_settings))
+            with open(rel_path, 'rb') as f:
+                res_settings_lst.append(pickle.load(f))
+        res_ret.append(res_settings_lst)
 
     return res_ret
 
@@ -78,9 +90,9 @@ if __name__ == "__main__":
     settings_collection = [
         fs.FastSLAMSettings(
             num_particles=n,
-        ) for n in [5, 10, 20, 40, 80]
+        ) for n in [5, 10]
     ]
 
-    sensor_data = sd.load_sensor_data('sim0.xz')
+    sensor_data = sd.load_sensor_data('sim5.xz')
 
-    res = slam_batch(settings_collection, sensor_data, repeats=5, pool_processes=4)
+    res = slam_batch(settings_collection, sensor_data, repeats=3, pool_processes=4)
