c18a6ba9624ed5194a105afd76a6174a45707580
José Pedro <zemiguel.apedro@gmail.com>
2022-06-25 10:47:02 +0100

    Fix bug in experiment script.

--- get_exps_script.py
+++ get_exps_script.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-N_default = 100
+N_default = 50
 r_odom_default = 0.1
 theta_odom_default = 0.5
 r_bias_default = 1
@@ -11,21 +11,21 @@ phi_line_default = 10*np.pi/180
 N_exp = " ".join([str(n) for n in [10, 20, 50, 100, 200]])
 
 action_model_noise_theta_bias_exp = [
-    [r_odom_default, theta_bias]
+    [r_bias_default, theta_bias]
     for theta_bias in np.linspace(0.1, 1.0, 10, endpoint=True)
 ]
 
 action_model_noise_r_bias_exp = [
-    [r_bias, theta_odom_default]
+    [r_bias, theta_bias_default]
     for r_bias in np.linspace(0.05, 0.5, 10, endpoint=True)
 ]
 
 action_model_noise_var_r_exp = [
-    [r_var, theta_bias_default]
+    [r_var, theta_odom_default]
     for r_var in np.linspace(0.8, 1.2, 5, endpoint=True)
 ]
 action_model_noise_var_theta_exp = [
-    [r_bias_default, theta_var]
+    [r_odom_default, theta_var]
     for theta_var in np.linspace(0.8, 1.2, 5, endpoint=True)
 ]
 
