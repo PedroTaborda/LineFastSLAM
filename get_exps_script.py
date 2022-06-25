073175fc98be844ea76e8c4626e29c0083937b15
José Pedro <zemiguel.apedro@gmail.com>
2022-06-25 11:04:43 +0100

    Experience.

--- get_exps_script.py
+++ get_exps_script.py
@@ -12,26 +12,26 @@ N_exp = " ".join([str(n) for n in [10, 20, 50, 100, 200]])
 
 action_model_noise_theta_bias_exp = [
     [r_bias_default, theta_bias]
-    for theta_bias in np.linspace(0.1, 1.0, 10, endpoint=True)
+    for theta_bias in np.linspace(0.8, 1.2, 5, endpoint=True)
 ]
 
 action_model_noise_r_bias_exp = [
     [r_bias, theta_bias_default]
-    for r_bias in np.linspace(0.05, 0.5, 10, endpoint=True)
+    for r_bias in np.linspace(0.8, 1.2, 5, endpoint=True)
 ]
 
 action_model_noise_var_r_exp = [
     [r_var, theta_odom_default]
-    for r_var in np.linspace(0.8, 1.2, 5, endpoint=True)
+    for r_var in np.linspace(0.05, 0.5, 10, endpoint=True)
 ]
 action_model_noise_var_theta_exp = [
     [r_odom_default, theta_var]
-    for theta_var in np.linspace(0.8, 1.2, 5, endpoint=True)
+    for theta_var in np.linspace(0.1, 1.0, 10, endpoint=True)
 ]
 
-r_line = " ".join([f"{r_line_val:.7f}" for r_line_val in np.linspace(0.8, 1.2, 5, endpoint=True)])
+r_line = " ".join([f"{r_line_val:.7f}" for r_line_val in np.linspace(0.05, 0.5, 10, endpoint=True)])
 
-phi_line = " ".join([f"{phi_line_val:.7f}" for phi_line_val in np.linspace(0.8, 1.2, 5, endpoint=True)])
+phi_line = " ".join([f"{phi_line_val:.7f}" for phi_line_val in np.linspace(2*np.pi/180, 20*np.pi/180, 10, endpoint=True)])
 
 run_str_exp1 = f'python3 -m slam.mass --sensor-data corridor-w-light.xz -t0 20 \
 -N {N_exp} --action-model-noise-cov "[[{r_odom_default}, {theta_odom_default}]]" \
